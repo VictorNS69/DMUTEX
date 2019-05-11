ddc5bc94dc05b023a0bc5b34250d6056dfc4b262
Victor Nieves Sanchez <vnievess@gmail.com>
2019-05-11 23:01:14 +0200

    changed some names

--- src/Proceso/lib_message.h
+++ src/Proceso/lib_message.h
@@ -3,5 +3,5 @@
 int serialize(const MESSAGE *msg, unsigned char **buf, int *lclk, int n_peers);
 MESSAGE *deserialize(const unsigned char *buf, const size_t bufSz, int n_peers);
 int process_name(char *name, const int port, PEER *peers, int n_peers);
-int send_message(SOCKET *info, const char *to, int *lclk, int myIndex, PEER *peers, int n_peers);
-MESSAGE *receive_message(const SOCKET *info, char *pname, PEER *peers, int n_peers);
+int send_message(SOCKET *s, const char *to, int *lclk, int myIndex, PEER *peers, int n_peers);
+MESSAGE *receive_message(const SOCKET *s, char *pname, PEER *peers, int n_peers);
